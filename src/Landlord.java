d80f9456d129845d1cd012c541dd6c978ca9f2c0
jcdesimp <jcdesimp@aol.com>
2014-02-24 23:09:30 -0500

    Bukkit dependencies established

--- src/Landlord.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by jcdesimp on 2/24/14.
- */
-public class Landlord {
-    //Plugin
-}
---
+++ src/com/jcdesimp/landlord/Landlord.java
@@ -0,0 +1,16 @@
+package com.jcdesimp.landlord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by jcdesimp on 2/24/14.
+ */
+public class Landlord extends JavaPlugin {
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
