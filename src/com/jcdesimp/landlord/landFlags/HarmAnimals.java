899d3ed8f22b3cb035cc97a660101878565e01e1
Joseph DeSimpliciis <jcdesimp@aol.com>
2014-04-16 12:11:17 -0400

    Harm animals flag implemented.

--- src/com/jcdesimp/landlord/landFlags/HarmAnimals.java
+++ src/com/jcdesimp/landlord/landFlags/HarmAnimals.java
@@ -1,8 +1,17 @@
 package com.jcdesimp.landlord.landFlags;
 
 import com.jcdesimp.landlord.landManagement.Landflag;
+import com.jcdesimp.landlord.persistantData.OwnedLand;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 
 /**
  * File created by jcdesimp on 4/16/14.
@@ -63,6 +72,47 @@ public class HarmAnimals extends Landflag {
      *************************************
      */
 
+    @EventHandler(priority = EventPriority.HIGH)
+    public void animalKill(EntityDamageByEntityEvent event){
+        String[] safeAnimals = {"OCELOT","WOLF","HORSE","COW","PIG","MUSHROOM_COW","SHEEP","CHICKEN"};
+        org.bukkit.entity.Entity victim = event.getEntity();
+        if(!Arrays.asList(safeAnimals).contains(victim.getType().toString())){
+            return;
+        }
+
+
+        org.bukkit.entity.Entity attacker = event.getDamager();
+
+        if(attacker.getType().toString().equals("PLAYER")){
+            Player p = (Player)attacker;
+            OwnedLand land = OwnedLand.getApplicableLand(victim.getLocation());
+            if(!land.hasPermTo(p, this)){
+
+                    p.sendMessage(ChatColor.RED+"You cannot harm animals on this land.");
+
+                event.setCancelled(true);
+
+            }
+
+        } else if(attacker.getType().toString().equalsIgnoreCase("Arrow") || attacker.getType().toString().equalsIgnoreCase("SPLASH_POTION")){
+            Projectile a = (Projectile)attacker;
+            Player p;
+            if(a.getShooter() instanceof Player){
+                OwnedLand land = OwnedLand.getApplicableLand(victim.getLocation());
+                p = (Player)a.getShooter();
+                //System.out.println(a.getType());
+                if(!land.hasPermTo(p, this)){
+                    if(a.getType().toString().equals("ARROW")) {
+                        p.sendMessage(ChatColor.RED + "You cannot harm animals on this land.");
+                    }
+                    a.remove();
+                    event.setCancelled(true);
+                }
+            }
+
+
+        }
+    }
 
 
 
